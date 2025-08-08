d824076b15f1846edf240ff6a1d21d8348bf1394
Robin Steiner <robin.steiner@vorstieg.eu>
2025-08-08 16:13:54 +0200

    add finanz online integration

--- pos_registrierkasse/controllers/daten_erfassungs_protokoll.py
+++ pos_registrierkasse/controllers/daten_erfassungs_protokoll.py
@@ -6,7 +6,7 @@ import pytz
 from odoo import http
 from odoo.http import request, content_disposition
 
-from odoo.addons.pos_registrierkasse.models.utils.a_trust_library import OrderData
+from odoo.addons.pos_registrierkasse.models.libs.a_trust.a_trust_library import OrderData
 
 from odoo.addons.pos_registrierkasse.models.utils.order_utils import jws_signature_compact
 
--- pos_registrierkasse/models/__init__.py
+++ pos_registrierkasse/models/__init__.py
@@ -3,3 +3,4 @@
 from . import pos_order
 from . import pos_config
 from . import utils
+from . import res_config_settings
